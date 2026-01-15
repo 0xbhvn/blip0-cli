71630c380ea0a220630189706f459bd839b16b74
Bhaven Kakade <thisisbhaven@gmail.com>
2026-01-16 01:45:39 +0530

    refactor(commitlint): standardize configuration formatting
    - Reformatted the commitlint configuration file for consistent indentation and style.
    - No functional changes were made; this enhances readability and maintainability of the configuration.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,25 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'ci',
-        'build',
-        'revert'
-      ]
-    ],
-    'subject-case': [0],
-    'body-max-line-length': [0],
-    'header-max-length': [2, 'always', 100]
-  }
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat",
+				"fix",
+				"docs",
+				"style",
+				"refactor",
+				"perf",
+				"test",
+				"chore",
+				"ci",
+				"build",
+				"revert",
+			],
+		],
+		"subject-case": [0],
+		"body-max-line-length": [0],
+		"header-max-length": [2, "always", 100],
+	},
 };
